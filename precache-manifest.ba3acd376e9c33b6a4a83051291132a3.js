bfddb0d0332bcc7ed23da606198c204d02fcff90
Wetzel <manuel.wetzel@hpe.com>
2020-02-15 22:43:40 +0100

    s

---
+++ precache-manifest.ba3acd376e9c33b6a4a83051291132a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf0a47a00512de311e17",
+    "url": "/css/app.70ebd6d9.css"
+  },
+  {
+    "revision": "1dd2456344500733ee49",
+    "url": "/css/chunk-vendors.a4393e1d.css"
+  },
+  {
+    "revision": "520f13fe3a306d7260df7f1f9c97718d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf0a47a00512de311e17",
+    "url": "/js/app.934a33ae.js"
+  },
+  {
+    "revision": "1dd2456344500733ee49",
+    "url": "/js/chunk-vendors.4e8bfe48.js"
+  },
+  {
+    "revision": "b701feda5e2a9b397ba8dff4b8c87338",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.16129c39261752f621126b1d62a0af43.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.ba3acd376e9c33b6a4a83051291132a3.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /* eslint-disable */
 /**
@@ -32,16 +32,4 @@ self.addEventListener('message', (event) => {
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerRoute(new RegExp('https:\/\/images.ctfassets.net\/kfibk3xh1vwb\/B5SY5CYcZxbmlZWZ0XCcJ\/efed3c1a56081b66d4a9fcc469c8d5f2\/*'), new workbox.strategies.StaleWhileRevalidate({ "cacheName":"images", plugins: [] }), 'GET');
-// workbox.routing.registerRoute(
-//   /\.(?:png|gif|jpg|jpeg|webp|svg)$/,
-//   new workbox.strategies.CacheFirst({
-//     'cacheName': 'images1',
-//     plugins: [
-//       new ExpirationPlugin({
-//         maxEntries: 60,
-//         maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-//       }),
-//     ],
-//   })
-// );
+workbox.routing.registerRoute(new RegExp('https:\/\/images.ctfassets.net\/kfibk3xh1vwb\/B5SY5CYcZxbmlZWZ0XCcJ\/efed3c1a56081b66d4a9fcc469c8d5f2\/*'), new workbox.strategies.CacheFirst({ "cacheName":"images", plugins: [] }), 'GET');
