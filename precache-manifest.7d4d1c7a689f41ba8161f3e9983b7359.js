fda269c7b943de0cf68ed97af16a0fb625e9fbd7
Wetzel <manuel.wetzel@hpe.com>
2020-02-18 11:48:33 +0100

    s

---
+++ precache-manifest.7d4d1c7a689f41ba8161f3e9983b7359.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b346b83384a7d08181b5",
+    "url": "/css/app.70ebd6d9.css"
+  },
+  {
+    "revision": "babcc12f869c40464a96",
+    "url": "/css/chunk-vendors.a4393e1d.css"
+  },
+  {
+    "revision": "81fb13920a25b10a0cdc267645c2d34f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b346b83384a7d08181b5",
+    "url": "/js/app.d33fd472.js"
+  },
+  {
+    "revision": "babcc12f869c40464a96",
+    "url": "/js/chunk-vendors.ef61e531.js"
+  },
+  {
+    "revision": "b701feda5e2a9b397ba8dff4b8c87338",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,8 @@
+importScripts("/precache-manifest.7d4d1c7a689f41ba8161f3e9983b7359.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/* eslint-disable */
 /**
- * Welcome to your Workbox-powered service worker!
+ * Welcome to your Workbox-powered service worker1!
  *
  * You'll need to register this file in your web app and you should
  * disable HTTP caching for this file too.
@@ -11,13 +14,9 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+workbox.setConfig({ debug: true });
 
-importScripts(
-  "/precache-manifest.42217b064197f5acb74d335cff0e0bff.js"
-);
-
-workbox.core.setCacheNameDetails({prefix: "kundeninformationssystem"});
+workbox.core.setCacheNameDetails({ prefix: "kundeninformationssystem" });
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
@@ -32,3 +31,5 @@ self.addEventListener('message', (event) => {
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerRoute(new RegExp('https:\/\/images.ctfassets.net\/kfibk3xh1vwb\/*'), new workbox.strategies.CacheFirst({ "cacheName": "images", plugins: [] }), 'GET');
