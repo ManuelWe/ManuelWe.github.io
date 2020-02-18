de5aeb06253dedd74671b879cd630baea5a19c21
Wetzel <manuel.wetzel@hpe.com>
2020-02-18 12:31:32 +0100

    s

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,5 @@
-importScripts("/precache-manifest.344843693157b54e12f08eee1f5ac515.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
-
-/* eslint-disable */
 /**
- * Welcome to your Workbox-powered service worker1!
+ * Welcome to your Workbox-powered service worker!
  *
  * You'll need to register this file in your web app and you should
  * disable HTTP caching for this file too.
@@ -14,9 +11,13 @@ importScripts("/precache-manifest.344843693157b54e12f08eee1f5ac515.js", "https:/
  * See https://goo.gl/2aRDsh
  */
 
-workbox.setConfig({ debug: true });
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
-workbox.core.setCacheNameDetails({ prefix: "kundeninformationssystem" });
+importScripts(
+  "/precache-manifest.8b7158aec5053af1c4ebcbffafc8c84e.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "test"});
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
@@ -31,5 +32,3 @@ self.addEventListener('message', (event) => {
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
-
-workbox.routing.registerRoute(new RegExp('https:\/\/images.ctfassets.net\/kfibk3xh1vwb\/*'), new workbox.strategies.CacheFirst({ "cacheName": "images", plugins: [] }), 'GET');
