f69991784a8aa68256929004eb64940e32a0d84a
Wetzel <manuel.wetzel@hpe.com>
2020-02-18 12:57:19 +0100

    s

---
+++ precache-manifest.845a1eedc26cc8d1f387c78a69c64850.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff239eae7252c7768699",
+    "url": "/css/app.70ebd6d9.css"
+  },
+  {
+    "revision": "babcc12f869c40464a96",
+    "url": "/css/chunk-vendors.a4393e1d.css"
+  },
+  {
+    "revision": "b235dbcc54d8dc6a69d46c63151408bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff239eae7252c7768699",
+    "url": "/js/app.533abe67.js"
+  },
+  {
+    "revision": "babcc12f869c40464a96",
+    "url": "/js/chunk-vendors.ef61e531.js"
+  },
+  {
+    "revision": "b701feda5e2a9b397ba8dff4b8c87338",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.344843693157b54e12f08eee1f5ac515.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.845a1eedc26cc8d1f387c78a69c64850.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /* eslint-disable */
 /**
@@ -33,3 +33,4 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerRoute(new RegExp('https:\/\/images.ctfassets.net\/kfibk3xh1vwb\/*'), new workbox.strategies.CacheFirst({ "cacheName": "images", plugins: [] }), 'GET');
+workbox.routing.registerRoute('https://cdn.contentful.com/spaces/kfibk3xh1vwb/entries?content_type=produkt', new workbox.strategies.CacheFirst({ "cacheName": "products", plugins: [] }), 'GET');
