c39735c6e511ab81d65b3844bc6b890eebc26bdc
Wetzel <manuel.wetzel@hpe.com>
2020-02-20 16:17:34 +0100

    s

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.3dc3efe1336c578b0d2a5ef68f2e8531.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.0eb4399e3fe1f662d00002107562917f.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /* eslint-disable */
 /**
