1dac86e4b9dc4707924f04cbd036259f471c6cb9
ManuelWe <34963919+ManuelWe@users.noreply.github.com>
2020-03-11 21:26:48 +0000

    Deploying to master from  - 455172d81d39a0f81da30c45b2a3cf6189b1c1e6 🚀

---
+++ precache-manifest.6876a14da2000399904d236ea8d9ba78.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54fb7665004798ef82c1",
+    "url": "/css/app.841c8a6b.css"
+  },
+  {
+    "revision": "316c64dcca3cacad37c1",
+    "url": "/css/chunk-0feccddc.3c8318c2.css"
+  },
+  {
+    "revision": "b7e8d4b83a1a9be73190",
+    "url": "/css/chunk-69092eec.120e12d2.css"
+  },
+  {
+    "revision": "279075e0dc4cbefe473a",
+    "url": "/css/chunk-a31cdf02.d6477e3e.css"
+  },
+  {
+    "revision": "c68037a7fb34da49a556",
+    "url": "/css/chunk-a584ed64.4ad6129a.css"
+  },
+  {
+    "revision": "c3525eca8337714c7364",
+    "url": "/css/chunk-vendors.a4393e1d.css"
+  },
+  {
+    "revision": "c8617f7bc15b7d9d71195cebe8aa2e75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54fb7665004798ef82c1",
+    "url": "/js/app.8259715d.js"
+  },
+  {
+    "revision": "316c64dcca3cacad37c1",
+    "url": "/js/chunk-0feccddc.00392363.js"
+  },
+  {
+    "revision": "4a0e508046d0166c7066",
+    "url": "/js/chunk-10e804e8.f5eec4c1.js"
+  },
+  {
+    "revision": "fa718ae7fa08a80f50ce",
+    "url": "/js/chunk-2d22b933.3b89edba.js"
+  },
+  {
+    "revision": "8a5ab31aaf9e9b4e45b0",
+    "url": "/js/chunk-5c05b752.0dc49075.js"
+  },
+  {
+    "revision": "b7e8d4b83a1a9be73190",
+    "url": "/js/chunk-69092eec.043ea187.js"
+  },
+  {
+    "revision": "279075e0dc4cbefe473a",
+    "url": "/js/chunk-a31cdf02.fe5790f1.js"
+  },
+  {
+    "revision": "c68037a7fb34da49a556",
+    "url": "/js/chunk-a584ed64.de065309.js"
+  },
+  {
+    "revision": "c3525eca8337714c7364",
+    "url": "/js/chunk-vendors.e2e046fc.js"
+  },
+  {
+    "revision": "b701feda5e2a9b397ba8dff4b8c87338",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,24 @@
+importScripts("/precache-manifest.6876a14da2000399904d236ea8d9ba78.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/* eslint-disable */
+/**
+ * Welcome to your Workbox-powered service worker1!
+ */
+
+workbox.setConfig({ debug: true }); //TODO entfernen
+
+workbox.core.setCacheNameDetails({ prefix: "kundeninformationssystem" });
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+workbox.precaching.precacheAndRoute([{url: 'https://use.fontawesome.com/releases/v5.3.1/js/all.js', revision: 'null' }]);
+
+workbox.routing.registerRoute(new RegExp('https:\/\/images.ctfassets.net\/kfibk3xh1vwb\/*'), new workbox.strategies.StaleWhileRevalidate({ "cacheName": "images", plugins: [] }), 'GET');
+workbox.routing.registerRoute('https://cdn.contentful.com/spaces/kfibk3xh1vwb/entries?content_type=produkt', new workbox.strategies.StaleWhileRevalidate({ "cacheName": "products", plugins: [] }), 'GET');
+workbox.routing.registerRoute('https://cdn.contentful.com/spaces/kfibk3xh1vwb/assets', new workbox.strategies.StaleWhileRevalidate({ "cacheName": "assets", plugins: [] }), 'GET');
